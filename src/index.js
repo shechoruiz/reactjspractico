1aac4d7baa73f35e1411040077e31c1b2a5f39e9
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2019-10-10 23:32:47 -0500

    Componentes, ciclos de vida y eventos

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import HolaMundo from './components/HolaMundo';
+import Button from './components/Button'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<HolaMundo />, document.getElementById('root'));
+ReactDOM.render(<Button text="Click 2" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
